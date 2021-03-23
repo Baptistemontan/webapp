aa55d90e046e789bc985509e956fcde8670a4961
Bamontan <baptiste.demontangon@gmail.com>
2021-03-23 13:55:29 +0100

    moved app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import './App.css';
+import './css/App.css';
 import DriverList from './components/DriverList';
 import DriverPage from './components/DriverPage';
 import { Driver } from './types';
