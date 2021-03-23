e304202c319b263ff94b2523e8907fa9bb27e735
Bamontan <baptiste.demontangon@gmail.com>
2021-03-23 13:08:54 +0100

    added comments

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,38 @@ import { Driver } from './types';
 
 
 function App() {
+	// hooks creation
 	const [drivers, setDrivers]:[Driver[], (drivers:Driver[]) => void] = useState<Driver[]>([]);
 	const [selected, setSelected]:[number, (id:number) => void] = useState<number>(-1);
+
+	// json parsing
 	const getDrivers = (path:string) => {
 		fetch(path)
 		.then(response => response.json())
 		.then(myJson => setDrivers(myJson))
 	}
+
+	// json fetching on reload
 	useEffect(()=>{
     	getDrivers("./drivers.json");
-	},[selected])
+	},[])
+
+	// click handler
 	const selectedClickHandler = (id:number) => {
 		setSelected(id);
 	}
-  return (
-	  <div className="app-wrapper">
-		  <div className="app-driverList">
-			  <DriverList drivers={drivers} clickHandler={selectedClickHandler}></DriverList>
-		  </div>
-		  <div className="app-driverPage">
-			  <DriverPage driver={drivers[selected]}></DriverPage>
-		  </div>
-	  </div>
-  );
+
+	// rendering
+	return (
+		<div className="app-wrapper">
+			<div className="app-driverList">
+				<DriverList drivers={drivers} clickHandler={selectedClickHandler}></DriverList>
+			</div>
+			<div className="app-driverPage">
+				<DriverPage driver={drivers[selected]}></DriverPage>
+			</div>
+		</div>
+	);
 }
 
 export default App;
