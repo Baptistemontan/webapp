993f2de48b4ce67d302b1955a2ef39e22125f623
Bamontan <baptiste.demontangon@gmail.com>
2021-03-23 16:20:26 +0100

    error check fort json loading

--- src/loadJson.ts
+++ src/loadJson.ts
@@ -3,6 +3,12 @@ import {Handler} from "./types"
 // Json loading and parsing
 export default function LoadJson(path:string, callback:Handler<any>) {
 	fetch(path)
-	.then(response => response.json())
-	.then((myJson) => callback(myJson))
+	.then(response => {
+		return response.json()
+	}, reason => {
+		console.log(reason);
+	})
+	.then((myJson) => {
+		callback(myJson)
+	})
 }
