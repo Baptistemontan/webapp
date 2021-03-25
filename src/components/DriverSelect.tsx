9a3a213379f619f5ef21105de545e9bfbfee7ca4
Bamontan <baptiste.demontangon@gmail.com>
2021-03-25 22:52:43 +0100

    added wrapper for display control

--- src/components/DriverSelect.tsx
+++ src/components/DriverSelect.tsx
@@ -5,10 +5,14 @@ import {Driver, Handler} from '../types'
 export default function DriverSelect({driver, clickHandler}:{driver:Driver, clickHandler:Handler<never>}) {
     return(
         <div className="DriverSelect-wrapper" onClick={clickHandler}>
-            <p className="DriverSelect-name">
-                {driver.name}
-            </p>
-            <div className={"DriverSelect-active-indicator " + (driver.active ? "DriverSelect-active" : "DriverSelect-inactive")}></div>
+            <div className="DriverSelect-name-wrapper">
+                <p className="DriverSelect-name">
+                    {driver.driverName}
+                </p>
+            </div>
+            <div className="DriverSelect-indicator-wrapper">
+                <div className={"DriverSelect-active-indicator " + (driver.active ? "DriverSelect-active" : "DriverSelect-inactive")}></div>
+            </div>
         </div>
     )
 }
