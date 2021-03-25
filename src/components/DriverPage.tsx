be8aa345b16985ce1b58911135a1b908db5006d1
Bamontan <baptiste.demontangon@gmail.com>
2021-03-25 22:52:04 +0100

    added google map rendering

--- src/components/DriverPage.tsx
+++ src/components/DriverPage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import "../css/DriverPage.css";
 import {Driver} from '../types'
+import MapContainer from './MapContainer'
 
-export default function DriverPage({driver}:{driver:Driver|undefined}) {
+export default function DriverPage({driver}:{driver:Driver | undefined}) {
     return(
         <div className="DriverPage-wrapper">
             <p>
-                {driver === undefined ? "no driver selected" : driver.name}
+                {driver === undefined ? "No driver selected." : driver.driverName}
             </p>
+            <div className="driverMap-wrapper">
+                <MapContainer height="50%" width="80%" driver={driver}></MapContainer>
+            </div>
         </div>
     )
 }
