e616b80a341bc1385a94e0195347c91d3474d345
Bamontan <baptiste.demontangon@gmail.com>
2021-03-27 00:23:06 +0100

    finally find how to custom icons and center them

--- src/components/DriverMarker.tsx
+++ src/components/DriverMarker.tsx
@@ -2,19 +2,25 @@ import React, { useState } from 'react';
 import { Driver, Handler2 } from '../types';
 import { Marker, InfoWindow} from 'react-google-maps';
 
+const Pin = {url:"http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png", origin:{x:-10,y:0}};
+
 export default function DriverMarker({driver, clickHandler}:{driver:Driver, clickHandler:Handler2<Driver, boolean>}) {
-    const [markerVisibility, setMarkerVisibility] = useState<boolean>(false);
+    const [infoWindowVisibility, setInfoWindowVisibility] = useState<boolean>(false);
     const onClick = () => {
-        setMarkerVisibility(!markerVisibility);
+        setInfoWindowVisibility(!infoWindowVisibility);
         clickHandler(driver, false);
     }
     return (
-        <Marker position={driver.events[0].pos} onClick={onClick}>
-            { markerVisibility && 
-                <InfoWindow onCloseClick={() => setMarkerVisibility(false)}>
+        <Marker 
+            position={driver.events[0].pos}
+            onClick={onClick}
+            icon={Pin}
+        >
+            { infoWindowVisibility && 
+                <InfoWindow onCloseClick={() => setInfoWindowVisibility(false)}>
                     <p>{driver.driverName}</p>
                 </InfoWindow>
             }
-          </Marker>
+        </Marker>
     )
 }
