ceb18bfc23499ea35cd5356d92bdfb531489cd92
Bamontan <baptiste.demontangon@gmail.com>
2021-03-27 00:23:31 +0100

    (baby) progress on multi page Info

--- src/components/DriverPage.tsx
+++ src/components/DriverPage.tsx
@@ -6,8 +6,17 @@ import MapWrapper from './MapWrapper'
 export default function DriverPage({recenter,currentDriver, drivers, changeDriverHandler}:{recenter:boolean, currentDriver:Driver | undefined, drivers:Driver[]|undefined, changeDriverHandler:Handler2<Driver|undefined, boolean>}) {
     return(
         <div className="DriverPage-wrapper">
-            <p>Driver Name : {currentDriver === undefined ? "No driver selected." : currentDriver.driverName} 
-            <br/>Truck ID : {currentDriver?.truckId}</p>
+            <div className="DriverPage-info-wrapper">
+                <div className="DriverPage-info">
+                    <p>Driver Name : {currentDriver === undefined ? "No driver selected." : currentDriver.driverName} 
+                    <br/>Truck ID : {currentDriver?.truckId}</p>
+                </div>
+                <div className="DriverPage-switch-button">
+                    <p>
+                        Driver Info Page
+                    </p>
+                </div>
+            </div>
             <div className="driverMap-wrapper">
                 <MapWrapper recenter={recenter} currentDriver={currentDriver} drivers={drivers} clickHandler={changeDriverHandler}/>
             </div>
