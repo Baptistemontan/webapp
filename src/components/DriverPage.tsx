dbb47517aed2eb2ec725dc41a998e6e2c50f058f
Bamontan <baptiste.demontangon@gmail.com>
2021-03-27 14:45:42 +0100

    changed accordingly to new Map component

--- src/components/DriverPage.tsx
+++ src/components/DriverPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../css/DriverPage.css";
 import {Driver, Handler2} from '../types'
-import MapWrapper from './MapWrapper'
+import Map, {MapStyle} from './Map'
 
 export default function DriverPage({recenter,currentDriver, drivers, changeDriverHandler}:{recenter:boolean, currentDriver:Driver | undefined, drivers:Driver[]|undefined, changeDriverHandler:Handler2<Driver|undefined, boolean>}) {
     return(
@@ -11,14 +11,14 @@ export default function DriverPage({recenter,currentDriver, drivers, changeDrive
                     <p>Driver Name : {currentDriver === undefined ? "No driver selected." : currentDriver.driverName} 
                     <br/>Truck ID : {currentDriver?.truckId}</p>
                 </div>
-                <div className="DriverPage-switch-button">
+                <div className="DriverPage-switch-button button">
                     <p>
                         Driver Info Page
                     </p>
                 </div>
             </div>
             <div className="driverMap-wrapper">
-                <MapWrapper recenter={recenter} currentDriver={currentDriver} drivers={drivers} clickHandler={changeDriverHandler}/>
+                <Map  recenter={recenter} currentDriver={currentDriver} drivers={drivers} clickHandler={changeDriverHandler} {...MapStyle}/>
             </div>
         </div>
     )
