bfccea03779cd4cf7bb59071c1473051b3772a8f
Bamontan <baptiste.demontangon@gmail.com>
2021-03-27 14:45:03 +0100

    back to normal marker icon

--- src/components/DriverMarker.tsx
+++ src/components/DriverMarker.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Driver, Handler2 } from '../types';
 import { Marker, InfoWindow} from 'react-google-maps';
 
-const Pin = {url:"http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png", origin:{x:-10,y:0}};
+// const Pin = {url:"http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png", origin:{x:-10,y:0}};
 
 export default function DriverMarker({driver, clickHandler}:{driver:Driver, clickHandler:Handler2<Driver, boolean>}) {
     const [infoWindowVisibility, setInfoWindowVisibility] = useState<boolean>(false);
@@ -14,7 +14,6 @@ export default function DriverMarker({driver, clickHandler}:{driver:Driver, clic
         <Marker 
             position={driver.events[0].pos}
             onClick={onClick}
-            icon={Pin}
         >
             { infoWindowVisibility && 
                 <InfoWindow onCloseClick={() => setInfoWindowVisibility(false)}>
