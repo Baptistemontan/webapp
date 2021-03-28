84c1c8572d9ff2bc642130d3519b6ee4659d5462
Bamontan <baptiste.demontangon@gmail.com>
2021-03-28 22:27:03 +0200

    proper display of event

--- src/components/DriverInfo.tsx
+++ src/components/DriverInfo.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/DriverPage.css";
-import { Driver, Handler } from '../types'
+import { Driver, DriverEvent, Handler } from '../types'
+import DriverEvents from "./DriverEvents"
+import { timestampToDateStr } from "../functions"
+
+// no typescript version of this package, but it does exactly what I need so im gonna use it anyway
+const formatCoords = require('formatcoords')
+
+
+function EventInfo({event}:{event:DriverEvent}) {
+    return(
+        <div>
+            <p>Date : {timestampToDateStr(event.eventTime)}</p>
+            <p>Route ID : {event.routeId}</p>
+            <p>Route Name : {event.routeName}</p>
+            <p>Speed : {event.speed}</p>
+            <p>Event type : {event.eventType}</p>
+            <p>Foggy : {event.foggy ? "True" : "False"}</p>
+            <p>Rainy : {event.rainy ? "True" : "False"}</p>
+            <p>Windy : {event.windy ? "True" : "False"}</p>
+            <p>Congestion level : {event.congestionLevel}</p>
+            <p>Event position : {formatCoords(event.pos.lat, event.pos.lng).format()}</p>
+        </div>
+    )
+}
 
 export default function DriverInfo({currentDriver, changePageHandler}:{currentDriver:Driver, changePageHandler:Handler<never>}) {
+    const [currentEvent, setCurrentEvent] = useState<DriverEvent|undefined>(undefined)
     return(
         <div className="DriverPage-wrapper">
             <div className="DriverPage-info-wrapper">
@@ -16,6 +40,8 @@ export default function DriverInfo({currentDriver, changePageHandler}:{currentDr
                     </p>
                 </div>
             </div>
+            <DriverEvents currentDriver={currentDriver} eventSelectHandle={setCurrentEvent} />
+            {currentEvent && <EventInfo event={currentEvent}/>}
         </div>
     )
 }
