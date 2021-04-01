b74d730ab565517a104c0a997a9b0c6019a50249
Bamontan <baptiste.demontangon@gmail.com>
2021-04-02 01:01:35 +0200

    removed useless spaces

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -10,15 +10,15 @@ const classSma = "driverEvent-small";
 function DriverEventDisplay({event, clickHandle, selected}:{event:DriverEvent, clickHandle:Handler<never>, selected:boolean}) {
     return(
         <tr onClick={clickHandle} className={"button " + (selected ? "selected" : "")}>
-            <td className={classBig}>{timestampToDateStr(event.eventTime)}  </td>
-            <td className={classSma}>{event.routeId}                        </td>
-            <td className={classBig}>{event.routeName}                      </td>
-            <td className={classSma}>{event.speed}                          </td>
-            <td className={classBig}>{event.eventType}                      </td>
-            <td className={classSma}>{event.foggy ? "True" : "False"}       </td>
-            <td className={classSma}>{event.rainy ? "True" : "False"}       </td>
-            <td className={classSma}>{event.windy ? "True" : "False"}       </td>
-            <td className="driverEvent-last">{event.congestionLevel}        </td>
+            <td className={classBig}>{timestampToDateStr(event.eventTime)}</td>
+            <td className={classSma}>{event.routeId}</td>
+            <td className={classBig}>{event.routeName}</td>
+            <td className={classSma}>{event.speed}</td>
+            <td className={classBig}>{event.eventType}</td>
+            <td className={classSma}>{event.foggy ? "True" : "False"}</td>
+            <td className={classSma}>{event.rainy ? "True" : "False"}</td>
+            <td className={classSma}>{event.windy ? "True" : "False"}</td>
+            <td className="driverEvent-last">{event.congestionLevel}</td>
         </tr>
     )
 }
@@ -52,21 +52,21 @@ export default function EventsList({currentEvent, eventSelectHandle, currentDriv
         <table className="noselect">
             <thead>
                 <tr className="driverEvent-Title">
-                    <th className={classBig} onClick={() => SortBy(driverEventTimeComp)}><div>Date :                </div></th>
-                    <th className={classSma} onClick={() => SortBy(driverEventRouteIdComp)}><div>Route ID :            </div></th>
-                    <th className={classBig} onClick={() => SortBy(driverEventRouteNameComp)}><div>Route name :          </div></th>
-                    <th className={classSma} onClick={() => SortBy(driverEventSpeedComp)}><div>Speed :               </div></th>
-                    <th className={classBig} onClick={() => SortBy(driverEventTypeComp)}><div>Event type :          </div></th>
-                    <th className={classSma}><div>Foggy :               </div></th>
-                    <th className={classSma}><div>Rainy :               </div></th>
-                    <th className={classSma}><div>Windy :               </div></th>
-                    <th className={classBig} onClick={() => SortBy(driverEventCongestionLvlComp)}><div>Congestion level :    </div></th>
+                    <th className={classBig} onClick={() => SortBy(driverEventTimeComp)}><div>Date :</div></th>
+                    <th className={classSma} onClick={() => SortBy(driverEventRouteIdComp)}><div>Route ID :</div></th>
+                    <th className={classBig} onClick={() => SortBy(driverEventRouteNameComp)}><div>Route name :</div></th>
+                    <th className={classSma} onClick={() => SortBy(driverEventSpeedComp)}><div>Speed :</div></th>
+                    <th className={classBig} onClick={() => SortBy(driverEventTypeComp)}><div>Event type :</div></th>
+                    <th className={classSma}><div>Foggy :</div></th>
+                    <th className={classSma}><div>Rainy :</div></th>
+                    <th className={classSma}><div>Windy :</div></th>
+                    <th className={classBig} onClick={() => SortBy(driverEventCongestionLvlComp)}><div>Congestion level :</div></th>
                 </tr>
             </thead>
             <tbody>
                 { /* need this empty row to scroll back to the top */ }
                 <tr ref={topRowRef}></tr>
-                { /* same horrendous synthax error has in Map.tsx */ }
+                { /* same horrendous synthax error as in Map.tsx */ }
                 <>
                 {currentDriver.events.filter(driverEventPosFilter).sort(compWay(currentComp, reversedComp)).map((event, index) => (
                     <DriverEventDisplay key={event.eventTime + index} selected={event === currentEvent} event={event} clickHandle={() => eventSelectHandle(event) }/>
